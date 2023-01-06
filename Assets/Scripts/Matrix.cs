9c25d357f917c1f451ed77feb387dd71faeb0f5f
ivnvtosh <ivnvtosh@gmail.com>
2023-01-06 22:19:42 +0300

    Add matrix and vector

---
+++ Assets/Scripts/Matrix.cs
@@ -0,0 +1,218 @@
+public class Matrix
+{
+    public readonly int m;
+    public readonly int n;
+    private readonly float[,] _data;
+    
+    public Matrix(int m, int n = 1)
+    {
+        _data = new float[m, n];
+        this.m = m;
+        this.n = n;
+    }
+    
+    public float this[int i, int j]
+    {
+        get => _data[i, j];
+        set => _data[i, j] = value;
+    }
+    
+    public Matrix T
+    {
+        get
+        {
+            var c = new Matrix(n, m);
+
+            for (var i = 0; i < m; i += 1)
+            {
+                for (var j = 0; j < n; j += 1)
+                {
+                    c[j, i] = _data[i, j];
+                }
+            }
+
+            return c;
+        }
+    }
+    
+    public static Matrix Random(int m, int n)
+    {
+        var a = new Matrix(m, n);
+
+        for (var i = 0; i < m; i += 1)
+        {
+            for (var j = 0; j < n; j += 1)
+            {
+                a[i, j] = UnityEngine.Random.Range(-1.0f, 1.0f);
+            }
+        }
+        
+        return a;
+    }
+    
+    public override string ToString()
+    {
+        var description = "";
+
+        description += "m: " + m + "\n";
+        description += "n: " + n + "\n";
+        for (var i = 0; i < m; i += 1)
+        {
+            for (var j = 0; j < n - 1; j += 1)
+            {
+                description += _data[i, j] + " ";
+            }
+
+            description += _data[i, n - 1] + "\n";
+        }
+
+        return description;
+    }
+
+    public static Matrix operator *(float k, Matrix a)
+    {
+        var m = a.m;
+        var n = a.n;
+        var c = new Matrix(m, n);
+
+        for (var i = 0; i < m; i += 1)
+        {
+            for (var j = 0; j < n; j += 1)
+            {
+                c[i, j] = k * a[i, j];
+            }
+        }
+
+        return c;
+    }
+    
+    public static Matrix operator *(Matrix a, Vector b)
+    {
+        if (a.n != b.size)
+        {
+            throw new System.ArgumentException();
+        }
+        
+        var m = a.m;
+        var n = b.size;
+        var c = new Matrix(m, n);
+
+        for (var i = 0; i < m; i += 1)
+        {
+            for (var j = 0; j < n; j += 1)
+            {
+                for (var k = 0; k < a.n; k += 1)
+                {
+                    c[i, j] += a[i, k] * b[k];
+                }
+            }
+        }
+
+        return c;
+    }
+    
+    public static Matrix operator +(Matrix a, Matrix b)
+    {
+        if (a.n != b.n || a.m != b.m)
+        {
+            throw new System.ArgumentException();
+        }
+        
+        var m = a.m;
+        var n = a.n;
+        var c = new Matrix(m, n);
+
+        for (var i = 0; i < m; i += 1)
+        {
+            for (var j = 0; j < n; j += 1)
+            {
+                c[i, j] = a[i, j] + b[i, j];
+            }
+        }
+
+        return c;
+    }
+    
+    public static Matrix operator -(Matrix a, Matrix b)
+    {
+        if (a.n != b.n || a.m != b.m)
+        {
+            throw new System.ArgumentException();
+        }
+        
+        var m = a.m;
+        var n = a.n;
+        var c = new Matrix(m, n);
+
+        for (var i = 0; i < m; i += 1)
+        {
+            for (var j = 0; j < n; j += 1)
+            {
+                c[i, j] = a[i, j] - b[i, j];
+            }
+        }
+
+        return c;
+    }
+
+    public static Matrix operator *(Matrix a, Matrix b)
+    {
+        if (a.n != b.m)
+        {
+            throw new System.ArgumentException();
+        }
+        
+        var m = a.m;
+        var n = b.n;
+        var c = new Matrix(m, n);
+
+        for (var i = 0; i < m; i += 1)
+        {
+            for (var j = 0; j < n; j += 1)
+            {
+                for (var k = 0; k < a.n; k += 1)
+                {
+                    c[i, j] += a[i, k] * b[k, j];
+                }
+            }
+        }
+
+        return c;
+    }
+    
+    public override bool Equals(object obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+        {
+            return false;
+        }
+
+        var b = (Matrix) obj;
+
+        if (m != b.m || n != b.n)
+        {
+            throw new System.ArgumentException();
+        }
+
+        for (var i = 0; i < m; i += 1)
+        {
+            for (var j = 0; j < n; j += 1)
+            {
+                if (UnityEngine.Mathf.Approximately(_data[i, j], b[i, j]) == false)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public override int GetHashCode()
+    {
+        return System.Collections.Generic.EqualityComparer<float[,]>.Default.GetHashCode(_data) + 
+               System.Collections.Generic.EqualityComparer<int>.Default.GetHashCode(n) +
+               System.Collections.Generic.EqualityComparer<int>.Default.GetHashCode(m) +
+               System.Collections.Generic.EqualityComparer<Matrix>.Default.GetHashCode(this);
+    }
+}
---
+++ Assets/Scripts/Vector.cs
@@ -0,0 +1,163 @@
+public class Vector
+{
+    public readonly int size;
+    private readonly float[] _data;
+    
+    public Vector(float[] data)
+    {
+        size = data.Length;
+        _data = data;
+    }
+    
+    public Vector(int size)
+    {
+        this.size = size;
+        _data = new float[size];
+    }
+    
+    public float this[int i]
+    {
+        get => _data[i];
+        set => _data[i] = value;
+    }
+    
+    public Matrix T
+    {
+        get
+        {
+            var c = new Matrix(size, 1);
+
+            for (var i = 0; i < size; i += 1)
+            {
+                c[i, 0] = _data[i];
+            }
+
+            return c;
+        }
+    }
+    
+    public static Vector Random(int size)
+    {
+        var a = new Vector(size);
+
+        for (var i = 0; i < size; i += 1)
+        {
+            a[i] = UnityEngine.Random.Range(-1.0f, 1.0f);
+        }
+        
+        return a;
+    }
+    
+    public override string ToString()
+    {
+        var description = "";
+
+        description += "size: " + size + "\n";
+        for (var i = 0; i < size - 1; i += 1)
+        {
+            description += _data[i] + " ";
+        }
+        
+        description += _data[size - 1] + "\n";
+        return description;
+    }
+    
+    public static Vector operator *(float k, Vector b)
+    {
+        var size = b.size;
+        var c = new Vector(size);
+
+        for (var i = 0; i < size; i += 1)
+        {
+            c[i] = k * b[i];
+        }
+
+        return c;
+    }
+    
+    public static Vector operator +(Vector a, Vector b)
+    {
+        if (a.size != b.size)
+        {
+            throw new System.ArgumentException();
+        }
+
+        var size = a.size;
+        var c = new Vector(size);
+
+        for (var i = 0; i < size; i += 1)
+        {
+            c[i] = a[i] + b[i];
+        }
+
+        return c;
+    }
+    
+    public static Vector operator -(Vector a, Vector b)
+    {
+        if (a.size != b.size)
+        {
+            throw new System.ArgumentException();
+        }
+
+        var size = a.size;
+        var c = new Vector(size);
+
+        for (var i = 0; i < size; i += 1)
+        {
+            c[i] = a[i] - b[i];
+        }
+
+        return c;
+    }
+    
+    public static Vector operator *(Vector a, Vector b)
+    {
+        if (a.size != b.size)
+        {
+            throw new System.ArgumentException();
+        }
+
+        var size = a.size;
+        var c = new Vector(size);
+
+        for (var i = 0; i < size; i += 1)
+        {
+            c[i] = a[i] * b[i];
+        }
+
+        return c;
+    }
+    
+    public override bool Equals(object obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+        {
+            return false;
+        }
+
+        var b = (Vector) obj;
+        
+        if (size != b.size)
+        {
+            throw new System.ArgumentException();
+        }
+
+        for (var i = 0; i < size; i += 1)
+        {
+            if (UnityEngine.Mathf.Approximately(_data[i], b[i]) == false)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public override int GetHashCode()
+    {
+        return System.Collections.Generic.EqualityComparer<float[]>.Default.GetHashCode(_data) +
+               System.Collections.Generic.EqualityComparer<int>.Default.GetHashCode(size) + 
+               System.Collections.Generic.EqualityComparer<Vector>.Default.GetHashCode(this);
+    }
+}
